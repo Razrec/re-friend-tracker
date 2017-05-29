f6d18f65e5b5e7a61ebef9f50f6c0c44d4fa24cf
Razrec <raphael-lehmann@hotmail.com>
2017-05-29 12:15:45 +0200

    Add files via upload

---
+++ guimodel.ts
@@ -0,0 +1,419 @@
+export class GuiModel {
+
+    private _guiModel = {
+        "application": {
+            "title": "Requirements Engineering Friend Tracker Raphael Lehmann",
+            
+            "formList": [
+                {
+                    "id": "FriendForm",
+                    "title": "Friend",
+                    "formFieldList": [
+                        {
+                            "id": "familyName",
+                            "type": "text",
+                            "name": "FamilyName",
+                            "width": 1,
+                            "required": true
+                        },
+                        {
+                            "id": "firstName",
+                            "type": "text",
+                            "name": "FirstName",
+                            "width": 1,
+                            "required": true
+                        },
+                        {
+							 "id": "nickname",
+							 "type": "text",
+							 "name": "Nickname",
+							 "width": 2,
+							 "required": true
+						},
+						{
+							 "id": "group",
+							 "type": "autocomplete",
+							 "name": "Group",
+							 "data": [ "Study", "Family", "School" ],
+							 "form": "GroupForm",
+							 "width": 2
+						},  
+                        {
+                            "id":   "location",
+                            "type": "autocomplete",
+                            "name": "Location",
+                            "data": [ "Winterthur", "Zürich" ],
+                            "form": "GroupForm",
+                            "width": 2
+                        },
+                        {
+                            "id": "evtBirth",
+                            "type": "date",
+                            "name": "Birthday",
+                            "width": 2
+                        },
+                        {
+                            "id": "comment",
+                            "type": "text",
+                            "name": "Comments",
+                            "width": 2,
+                            "height": 4,
+                            "maxLength": 5000,
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+                            "type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                },
+                {
+                    "id": "LocationForm",
+                    "title": "Location",
+                    "formFieldList": [
+                        {
+                            "id": "name",
+                            "type": "text",
+                            "name": "LocationName",
+                            "width": 2,
+                            "required": true
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+                            "type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                },
+                {
+	                 "id": "GroupForm",
+					 "title": "Group",
+					 "formFieldList": [
+						 {
+							 "id": "name",
+							 "type": "text",
+							 "name": "GroupName",
+							 "width": 2,
+							 "required": true
+						 },
+						  {
+                            "id": "creationDate",
+                            "type": "date",
+                            "name": "CreationDate",
+                            "width": 2,
+                            "required": true
+                        },
+						 {
+							 "type": "deleteButton",
+							 "name": "Delete"
+						 },
+						 {
+							 "type": "cancelButton",
+							 "name": "Cancel"
+						 },
+						 {
+							 "type": "okButton",
+							 "name": "Ok"
+						 }
+					 ]
+					},
+					{
+                    "id": "AddActivityForm",
+                    "title": "Activity",
+                    "formFieldList": [
+                        {
+                            "id":   "location",
+                            "type": "autocomplete",
+                            "name": "Activity",
+                            "data": [ "Kino", "Abendessen", "Sport" ],
+                            "form": "ActivityForm",
+                            "width": 2
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+                            "type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                },
+                {
+                    "id": "ActivityForm",
+                    "title": "Activity",
+                    "formFieldList": [
+                        {
+                            "id": "name",
+                            "type": "text",
+                            "name": "Name",
+                            "width": 1,
+                            "required": true
+                        },
+                        {
+                            "id": "date",
+                            "type": "date",
+                            "name": "Date",
+                            "width": 1,
+                            "required": true
+                        },
+                        {
+                            "id":   "group",
+                            "type": "autocomplete",
+                            "name": "Location",
+                            "data": [ "Winterthur", "ZÃ¼rich" ],
+                            "required": true,
+                            "form": "LocationForm",
+                            "width": 2
+                        },
+                        {
+                            "id": "comment",
+                            "type": "text",
+                            "name": "Comments",
+                            "width": 2,
+                            "height": 4,
+                            "maxLength": 5000,
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+                            "type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                }            
+            ],
+            "pageList": [
+                {
+                    "id": "mainmenu",
+                    "name": "MainMenu",
+                    "elementList": [
+                        {
+                            "type": "button",
+                            "name": "Friends",
+                            "icon": "fa-user",
+                            "color": "blue",
+                            "page": "friendspage",
+                        },
+                        {
+                            "type": "button",
+                            "name": "Location",
+                            "icon": "fa-cubes",
+                            "color": "yellow",
+                            "page": "locationspage",
+                        },
+                        {
+							 "type": "button",
+							 "name": "Groups",
+							 "icon": "fa-weixin",
+							 "color": "wisteria",
+							 "page": "groupspage",
+						},
+						{
+                            "type": "button",
+                            "name": "Activities",
+                            "icon": "fa-calendar",
+                            "color": "orange",
+                            "page": "activitiespage",
+                        },
+                    ]
+                },
+                {
+                    "id": "friendspage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "NewFriend",
+                            "icon": "fa-user",
+                            "color": "green",
+                            "form": {
+                                "form": "FriendForm"
+                            }
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "AddActivity",
+                            "icon": "fa-calendar",
+                            "color": "green",
+                            "form": {
+                                "form": "AddActivityForm"
+                            }
+                        },
+                        {
+                            "type": "list",
+                            "icon": "fa-user",
+                            "color": "blue",
+                            "search": true,
+                            "data": [ { name: "Anton Amacker" }, { name: "Britta Beavers"} ],
+                            "form": {
+                                "form": "FriendForm"
+                            }
+                        },
+                    ]
+                },
+                {
+                    "id": "locationspage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "NewLocation",
+                            "icon": "fa-home",
+                            "color": "green",
+                            "form": {
+                                "form": "LocationForm"
+                            }
+                        },
+                        {
+                            "type": "list",
+                            "icon": "fa-home",
+                            "color": "blue",
+                            "search": true,
+                            "data": [ { name: "Adelboden" }, { name: "Winterthur" }, { name: "Zinal"}, { name: "Zürich"} ],
+                            "page": "locationpage",
+                        },
+                    ]
+                },
+                {
+                    "id": "locationpage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "EditLocation",
+                            "icon": "fa-home",
+                            "color": "green",
+                            "form": {
+                                "form": "LocationForm"
+                            }
+                        },
+                        {
+                            "type": "list",
+                            "icon": "fa-calendar",
+                            "color": "orange",
+                            "search": true,
+                            "data": [ { name: "Sport"} ],
+                        },
+                    ]
+                },
+                {
+					 "id": "groupspage",
+					 "elementList": [
+						 {
+						 "type": "backbutton",
+						 },
+						 {
+							 "type": "newButton",
+							 "name": "NewGroup",
+							 "icon": "fa-weixin",
+							 "color": "green",
+							 "form": {
+							 	"form": "GroupForm"
+							 }
+						 },
+						 {
+							"type": "list",
+							"icon": "fa-weixin",
+							"color": "wisteria",
+							"search": true,
+							"data": [ { name: "Study" }, { name: "Family" }, { name: "School"} ],
+							"form": {
+								"form": "GroupForm"
+							}
+						},
+						 
+						 
+					 ]
+				},
+				 {
+                    "id": "activitiespage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "New Activity",
+                            "icon": "fa-calendar",
+                            "color": "green",
+                            "form": {
+                                "form": "ActivityForm"
+                            }
+                        },
+                        {
+                            "type": "list",
+                            "icon": "fa-weixin",
+                            "color": "wisteria",
+                            "search": true,
+                            "data": [ { key: { key: "1" }, name: "Kino" }, { key: { key: "2" }, name: "Abendessen" }, { key: { key: "3" }, name: "Sport"} ],
+                            "page": "activitypage"
+                        },
+                    ]
+                },
+                {
+                    "id": "activitypage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "button",
+                            "name": "Edit Activity",
+                            "icon": "fa-calendar",
+                            "color": "green",
+                            "form": {
+                                "form": "ActivityForm"
+                            }
+                        },
+                        {
+                            "type": "list",
+                            "icon": "fa-user",
+                            "color": "blue",
+                            "search": true,
+                            "data": [ { key: { key: "1" }, name: "Anton Amacker" }, { key: { key: "2" }, name: "Britta Beavers"} ],
+                            "form": {
+                                "form": "AddActivityForm"
+                            }
+                        },
+                    ]
+                }
+            ]
+        }
+    };
+
+
+    get guiModel() {
+        return this._guiModel;
+    }
+}
